f6f8e4c7ff813408fc8f8ee79d64d13b630b3fe4
Vinay <you@example.com>
2022-08-07 07:21:45 +0530

    done

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -1,12 +1,6 @@
-import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Nav = () => {
-  const [ data, setData ] = useState({show:false})
-
-  const toggle = () =>{
-    data.show ? setData({show:false}) : setData({show:true})
-  }
  
   return (
       <div className='nav'>
@@ -22,23 +16,6 @@ const Nav = () => {
           </ul>
       </div>   
         
-       <div className='mobile__nav'>
-       {
-          data.show &&   <div className='nav'>
-          <ul>
-              <li><Link  to ="/">Home</Link></li>
-              <li><Link  to="/bollywood" >Bollywood</Link></li>
-              <li><Link  to="/technology">Technology</Link></li>
-              <li><Link  to="/hollywood">Hollywood</Link></li>
-              <li><Link  to="/fitness">Fitness</Link></li>
-              <li><Link  to="/food">Food</Link></li>
-          </ul>
-      </div> 
-        }
-       <button className='toogle__button' onClick={()=>toggle()}><i className='icon' class="fa-solid fa-2x fa-bars"></i></button>
-       </div>
-       
-    
     </div>
   )
 }
