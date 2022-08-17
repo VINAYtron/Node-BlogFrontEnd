d3f5d66d0df89982685deba2a8c49767b075c954
Vinay <you@example.com>
2022-08-17 18:03:00 +0530

    done

--- src/Pages/SingleBlogPage.js
+++ src/Pages/SingleBlogPage.js
@@ -5,7 +5,7 @@ import './singlePage.css'
 const SingleBlogPage = () => {
 const location = useLocation();
 const {title, img, fulldescription} = location.state;
-console.log(title)
+// console.log(title)
   return (
     <div className='single__page'>
         <h1>{title}</h1>
